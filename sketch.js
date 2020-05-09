bf19b62eee9d00209ecd3661eb531921040c94d2
shreesankeerth <63505242+shreesankeerth@users.noreply.github.com>
2020-05-09 14:32:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+function setup() {
+  createCanvas(800,400);
+ 
+ 
+}
+
+function draw() {
+  background(0,0,0);  
+  rect(250,200,30,400);
+rect(550,200,30,400);
+rect(450,250,30,400);
+rect(350,250,30,400);
+rect(400,300,30,400);
+rect(200,200,430,30);
+triangle(200,200,400,50,630,200);
+  drawSprites();
+}
